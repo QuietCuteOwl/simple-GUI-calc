452cd35ea296354fbdf04c170239070fa4c6fba7
QuietCuteOwl <217069674+QuietCuteOwl@users.noreply.github.com>
2025-07-01 17:47:45 +0530

    Initial commit

--- calculator.py
+++ calculator.py
@@ -1,8 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit,
-                             QPushButton, QHBoxLayout, QVBoxLayout)
+                             QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout)
 from PyQt5.Qt import Qt
-from unicodedata import digit
 
 
 
@@ -10,48 +9,69 @@ from unicodedata import digit
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
-        self.vbox1 = QVBoxLayout()
-        self.vbox2 = QVBoxLayout()
-        self.vbox3 = QVBoxLayout()
-        self.vbox4 = QVBoxLayout()
-        self.vbox5 = QVBoxLayout()
+        self.grid = QGridLayout()
         self.buttons = []
-        self.ops_buttons = []
         self.arth_buttons = ['+', '-', '×', '÷', '=']
         for num in range(10):
             button = QPushButton(str(num), self)
             button.clicked.connect(self.collect)
             self.buttons.append(button)
 
-            if num in [1, 4, 7]:
-                self.vbox1.addWidget(button)
-            elif num in [2, 5, 8]:
-                self.vbox2.addWidget(button)
-            elif num in [3, 6, 9]:
-                self.vbox3.addWidget(button)
+            if num == 1:
+                self.grid.addWidget(button, 1, 0)
+            elif num == 2:
+                self.grid.addWidget(button, 1, 1)
+            elif num == 3:
+                self.grid.addWidget(button, 1, 2)
+            elif num == 4:
+                self.grid.addWidget(button, 2, 0)
+            elif num == 5:
+                self.grid.addWidget(button, 2, 1)
+            elif num == 6:
+                self.grid.addWidget(button, 2, 2)
+            elif num == 7:
+                self.grid.addWidget(button, 3, 0)
+            elif num == 8:
+                self.grid.addWidget(button, 3, 1)
+            elif num == 9:
+                self.grid.addWidget(button, 3, 2)
             elif num == 0:
-                self.vbox4.addWidget(button)
+                self.grid.addWidget(button, 4, 0)
+
         for sign in self.arth_buttons:
             if sign != "=":
                 arth_button = QPushButton(sign, self)
                 arth_button.clicked.connect(self.collect)
-                self.ops_buttons.append(arth_button)
-                self.vbox5.addWidget(arth_button)
+
+
+                if sign == '+':
+                    self.grid.addWidget(arth_button, 1, 3)
+                elif sign == '-':
+                    self.grid.addWidget(arth_button, 2, 3)
+                elif sign == '×':
+                    self.grid.addWidget(arth_button, 3, 3)
+                elif sign == '÷':
+                    self.grid.addWidget(arth_button, 4, 3)
             else:
                 arth_button = QPushButton(sign, self)
                 arth_button.clicked.connect(self.collect)
-                self.ops_buttons.append(arth_button)
-                self.vbox4.addWidget(arth_button)
+                self.grid.addWidget(arth_button, 4, 2)
+
+
         self.clear_button = QPushButton("C", self)
         self.clear_button.clicked.connect(self.collect)
         self.back_p = QPushButton("⌫", self)
-        self.back_p.clicked.connect(self.backspace)
+        self.back_p.clicked.connect(self.collect)
         self.decimal = QPushButton(".", self)
         self.decimal.clicked.connect(self.collect)
-        self.vbox3.addWidget(self.decimal)
-        self.vbox4.addWidget(self.back_p)
-        self.vbox4.addWidget(self.clear_button)
+
+        self.grid.addWidget(self.clear_button, 1, 4)
+        self.grid.addWidget(self.back_p, 2, 4)
+        self.grid.addWidget(self.decimal, 4, 1)
+
+
         self.ans_output = QLineEdit(self)
+        self.grid.addWidget(self.ans_output, 0, 0, 1, 3)
         self.first_time = True
         self.ops_list = []
         self.f_index = -1
@@ -60,103 +80,143 @@ class Calculator(QWidget):
         self.ans = ''
         self.fn_list = []
         self.ff_list = []
+
+        self.setLayout(self.grid)
         self.initUI()
     def initUI(self):
-
-        # for button in self.buttons:
-        #     button_name = int(button.text())
-        #     print(button_name)
-
-
-        vbox6 = QVBoxLayout()
-
-        vbox6.addWidget(self.ans_output)
-
-
-
-        hbox7 = QHBoxLayout()
-
-
-        hbox7.addLayout(self.vbox1)
-        hbox7.addLayout(self.vbox2)
-        hbox7.addLayout(self.vbox3)
-        hbox7.addLayout(self.vbox4)
-        hbox7.addLayout(self.vbox5)
-
-        vbox7 = QVBoxLayout()
-
-        vbox7.addLayout(vbox6)
-        vbox7.addLayout(hbox7)
-
-        self.setLayout(vbox7)
-
-
+        pass
 
     def collect(self, event):
+
+
         # if self.first_time:
         #     print("Connected")
         #     self.first_time = False
         sender = self.sender().text()
         print(f"{sender=} {self.ops_list=}")
-
+        self.temp_s()
         if sender == 'C':
             self.clear_func()
+        elif sender == '⌫':
+            self.backspace()
         else:
             self.verify(sender)
 
     def verify(self, sender):
-        print(f"LST_T: {self.ops_list}")
         self.collect_count += 1
         print(f'connect: {self.collect_count}')
+        self.temp_s()
+
         if sender != '=':
+            if sender in self.arth_buttons:
+                if sender == '×':
+                    sender = '*'
+                elif sender == '÷':
+                    sender = '/'
+                else:
+                    pass
+            else:
+                pass
             self.ops_list.append(sender)
+
             self.f_index += 1
             expression = ''.join(self.ops_list)
-            f_list = self.ops_list
-            self.fn_list = [i for i in f_list if i.isdigit()]
-            self.ff_list = [i for i in f_list if not i.isdigit()]
+            self.fn_list = [i for i in self.ops_list if i.isdigit()]
+            self.ff_list = [i for i in self.ops_list if not i.isdigit()]
 
-            if len(self.ff_list) >= 2:
-                if self.ff_list[-1] == self.ff_list[-2]:
-                    print(self.ff_list[-1])
-                    self.ops_list.pop()
-                    print("You cannot do the same operation twice")
-                    print(f"IDX: {self.f_index}")
+            if len(self.ff_list) >= 2:  #Checks if last two operators were same
+                ops_l = self.ops_list[-1]
+                ops_sl = self.ops_list[-2]
+                if ops_sl in self.ff_list and ops_l in self.ff_list:
+                    if ops_sl == '+':
+                        if ops_l == '+':
+                            self.ops_list.pop()
+                        elif ops_l == '-':
+                            self.ops_list.pop(-2)
+                        elif ops_l == '*':
+                            self.ops_list.pop(-2)
+                        elif ops_l == '/':
+                            self.ops_list.pop(-2)
+                        else:
+                            pass
+                    elif ops_sl == '-':
+                        if ops_l == '-':
+                            self.ops_list.pop()
+                        elif ops_l == '+':
+                            self.ops_list.pop(-2)
+                        elif ops_l == '*':
+                            self.ops_list.pop(-2)
+                        elif ops_l == '/':
+                            self.ops_list.pop(-2)
+                        else:
+                            pass
+                    elif ops_sl == '*':
+                        if ops_l == '*':
+                            pass
+                        elif ops_l == '+':
+                            self.ops_list.pop(-2)
+                        elif ops_l == '-':
+                            pass
+                        elif ops_l == '/':
+                            self.ops_list.pop(-2)
+                        else:
+                            pass
+                    elif ops_sl == '/':
+                        if ops_l == '/':
+                            self.ops_list.pop()
+                        elif ops_l == '+':
+                            self.ops_list.pop(-2)
+                        elif ops_l == '-':
+                            pass
+                        elif ops_l == '*':
+                            self.ops_list.pop(-2)
+                        else:
+                            pass
+                    elif ops_sl == '.':
+                        if ops_l in self.arth_buttons:
+                            self.ops_list.pop(-2)
+
             else:
-                print(f"IDX: {self.f_index}")
-            print(f"LST: {self.ops_list}")
+                print(f"{self.f_index=}")
+                self.temp_s()
+
             self.display(expression)
-            print(f"VRY_I: {expression}")
         else:
-            print(self.ops_list)
-            if len(self.ops_list) <= 0:
-                self.ans_output.setText("Invalid")
+            if len(self.ops_list) <= 1: #Checks if '=' was pressed without anything else
+                self.ans_output.setText("ERROR:Invalid Expression")
                 return
             else:
+                ops_l = self.ops_list[-1]
+                ops_sl = self.ops_list[-2]
+
                 expression = ''.join(self.ops_list)
-                if self.ops_list and self.ops_list[-1] == '=':
+                if ops_l == '=':                    #Pops the '='
                     self.ops_list.pop()
-                if self.ops_list and self.ops_list[-1] in self.ff_list:
+                if ops_l in self.ff_list:           #Checks if the operator was right
+                    self.ops_list.pop()             #after a number without any number ahead of it
+                    expression = ''.join(self.ops_list)
                     self.clear_func()
-                    self.ans_output.setText("Error:Operator right after number")
-                else:
-                    expression = expression.replace('×', '*').replace('÷', '/')
-                    self.ff_list = []
-                    self.fn_list = []
-                    self.f_index = -1
-                    self.ff_index = -1
+
                     self.calculate(expression)
-                    print(f"VRY_E: {expression}")
-        print(len(self.ops_list))
+
+                else:
+                    self.clear_func()
+                    self.calculate(expression)
+                    print(f"FINAL: {expression}")
+                    self.temp_s()
+
+        print(f"LEN: {len(self.ops_list)}")
+        print("________________")
     def calculate(self, expression):
 
         try:
             self.ans = str(eval(expression))
+
             print(f"ANS: {self.ans}")
             print(f"TYPE: {type(self.ans)}")
+            self.temp_s()
             expression = self.ans
             self.display(expression)
-            print(f"CALC:{expression}")
             self.ops_list = []
             for i in self.ans:
                 self.ops_list.append(i)
@@ -184,9 +244,8 @@ class Calculator(QWidget):
         self.ans = ''
         self.fn_list.clear()
         self.ff_list.clear()
-        self.f_index = 0
+        self.f_index = -1
         self.ff_index = -1
-        print(f"CLR2: {self.ops_list}")
 
     def backspace(self):
         if len(self.ops_list) < 1:
@@ -198,7 +257,7 @@ class Calculator(QWidget):
 
     def display(self, expression):
         print(f"Display: {expression}")
-
+        print(f"{self.ops_list}")
 
         self.ans_output.setText("")
         self.ans_output.setText(expression)
@@ -249,15 +308,19 @@ class Calculator(QWidget):
 
             if sender != '':
                 self.verify(sender)
-        else:
-            print(event.text())
-            print(event.key())
 
     @staticmethod
     def tepm():
         print("________________________________")
-if __name__ == '__main__':
+    @staticmethod
+    def temp_s():
+        print("____________")
+
+def main():
     app = QApplication(sys.argv)
     calculator = Calculator()
     calculator.show()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
